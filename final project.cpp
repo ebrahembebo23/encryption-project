fbd20142b84572d79cf29bf4b7e0d3eb52a56e5a
Ebrahem Karm Ebrahem Ahmed <bebohema23@gmail.com>
2021-08-24 10:38:22 +0200

    final project after edit

--- final project.cpp
+++ final project.cpp
@@ -1,11 +1,10 @@
 #include<stdio.h>
-	// Declaration and initialization global variable
-int  key = 3;
+
 	// make encryption function
-int  Encrypt()
+void  Encrypt(int key)
 {
 	char message[100], ch;
-    int i, key = 3;
+    int i;
     	// ask user to enter message to encrypt
     printf("Enter a message to encrypt: ");
     scanf("%s", message);
@@ -29,30 +28,26 @@ int  Encrypt()
     }
     	// print encrypted message
     printf("Encrypted message: %s\n", message);
-    return key;
+
     
 }
 
 
 	// make decreption function
-void  Decrypt()
+void  Decrypt(int key)
 {
 	char message[100], ch;
-    int i, yy;
+    int i, check;
     	// ask user to enter message to dycrept
     printf("Enter a message to decrypt: ");
     scanf("%s", message);
     	// ask user to enter the key
-    printf("Enter key: ");
-    scanf("%d", &yy);
-    while (yy != key)
+    printf("Enter the key: ");
+    scanf("%d", &check);
+    while (check != key)
     {
-    	if (yy == key)
-    	{
-    		break;
-		}
-    	printf("Error! thats wrong kay try again...\nEnter key:");
-    	scanf("%d", &yy);
+    	printf("Error! this is a wrong key. try again...\nEnter the key:");
+    	scanf("%d", &check);
 	}
     for(i = 0; message[i] != '\0'; ++i){
     ch = message[i];
@@ -77,51 +72,41 @@ void  Decrypt()
 }
 // if we need to make encryption and decreption use this function
 
-/*
-void Encrypt and Decrypt()
+
+void Encrypt_and_Decrypt(int key)
 
 {
-	int key = Encrypt();
-    printf("The message to decrypt: %s\n ", message);
-    for(i = 0; message[i] != '\0'; ++i){
-    ch = message[i];
-    if(ch >= 'a' && ch <= 'z'){
-    ch = ch - key;
-    if(ch > 'z'){
-    ch = ch - 'z' + 'a' - 1;
-    }
-    message[i] = ch;
-    }
-    else if(ch >= 'A' && ch <= 'Z'){
-    ch = ch - key;
-    if(ch > 'Z'){
-    ch = ch - 'Z' + 'A' - 1;
-    }
-    message[i] = ch;
-    }
-    }
-    printf("Encrypted message: %s\n", message);
-    
-}*/
+	Encrypt(key);
+	Decrypt(key);
+}
 
 // making menu function to make uset choise from it
 int menu(int key) 
 {
 	
 	int choice;
-	printf("Please choice one of the menu.\n1- Encrypt\n2- Decrypt\n3- Exit \nEnter your choise: ");
+	printf("Please choice one of the menu.\n1- Encrypt\n2- Decrypt\n3- Chang the key\n4- Exit \nEnter your choise: ");
 	scanf("%d", &choice);
+	if (choice == 4)
+	{
+		return 0;
+	}
 	switch (choice)
 	{
 		case 1:
-			key = Encrypt();
+		    Encrypt(key);
 			menu(key);
-		case 2:
-			
-			Decrypt();
+		break;
+		case 2:		
+			Decrypt(key);
 			menu(key);
-			
+		break;
 		case 3:
+		    printf("Enter the new key:");
+			scanf("%d", &key);	
+			menu(key);
+		break;			
+		case 4:
 		    return 0;
 	}
 
@@ -132,6 +117,8 @@ int main()
 {
 	//calling function
     int key;
+    printf("Enter the key for your Ecryption process: ");
+    scanf("%d", &key);	
 	menu(key);
 	
     return 0;
