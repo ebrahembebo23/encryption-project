702a9812c7b8c97d065efd4a2af372cbd4e6ceff
Ebrahem Karm Ebrahem Ahmed <bebohema23@gmail.com>
2021-08-25 21:56:17 +0200

    third way after edit

--- final project (RSA).cpp
+++ final project (RSA).cpp
@@ -3,47 +3,54 @@
 #include<math.h>
 #include<string.h>
 
-int p, q, n, Q, i, flag;
-long int e[50], d[50], temp[50], j, m[50], C[50];  //Long signed integer type. Capable of containing at least the [-2,147,483,647, +2,147,483,647] range
+int x, y, n, t, i, flag;
+long int e[50], d[50], temp[50], j, m[50], en[50];   //Long signed integer type. Capable of containing at least the [-2,147,483,647, +2,147,483,647] range
 char msg[100];
-int prime(long int); 
+int prime(long int);
 void encryption_key();
 long int cd(long int);
 void encrypt();
 void decrypt();
+int menu();
 
 int main()
 {
-  printf("\nEnter the first prime number:\n");
-  scanf("%d", &p);
-  flag = prime(p); // flag = 3
-  if(flag == 0)
-  {
-    printf("\nInvalid input");
-    exit(0);
-  }
-  printf("\nEnter the second prime number:");
-  scanf("%d", &q);
-  flag = prime(q);  // flag = 7
-  if(flag == 0 || q == p)
-  {
-    printf("\nInvalid input");
-    exit(0);
-  }
-  printf("\nEnter the message to encrypt: ");
-  scanf("%s",msg);
-  for(i = 0; msg[i] != NULL; i++)
-    m[i] = msg[i];
-  n = p * q;           // compute the key  (3*7 = 21)
-  Q = (p-1) * (q-1);  // compute the euler of the key  (2*6 =12)
-  Enc_RSA_key();     // key is (21,7)
-  /*printf("\nPOSSIBLE VALUES OF e AND d ARE\n");      if we wanna show how the values of e and d for each character we enterd 
-  for(i = 0; i < j-1; i++)
-    printf("\n%ld\t%ld", e[i], d[i]);*/
-  encrypt();  
-  decrypt();
+	encryption_key();
+    menu();
+ 
   return 0;
 }
+
+int menu()
+{
+	int choice;
+	printf("\nPlease choice one of the menu.\n1- Encrypt\n2- Decrypt\n3- Chang the key\n4- Exit \nEnter your choise: ");
+	scanf("%d", &choice);
+	if (choice == 4)
+	{
+		return 0;
+	}
+	switch (choice)
+	{
+		case 1:
+		    encrypt();
+		    menu(); 
+		break;
+		case 2:		
+			decrypt();
+			menu();
+		break;
+		case 3:
+		   encryption_key();
+		   menu();
+		break;			
+		case 4:
+		    return 0;
+	}
+ 
+}
+
+
 int prime(long int pr)
 {
   int i;
@@ -57,28 +64,46 @@ int prime(long int pr)
  }
 
 //function to generate encryption key
-void Enc_RSA_key()
+void encryption_key()
 {
-  int key_RSA = 0;
-  for(i = 2; i < Q; i++) // loop to know the possible values of e and d
-  {                      // e should be 1 < e < Q and comprime with Q(7)
-    if(Q % i == 0)
-        continue;
+	printf("\nENTER FIRST PRIME NUMBER\n");
+  scanf("%d", &x);
+  flag = prime(x);
+  if(flag == 0)
+  {
+    printf("\nINVALID INPUT\n");
+    exit(0);
+  }
+  printf("\nENTER SECOND PRIME NUMBER\n");
+  scanf("%d", &y);
+  flag = prime(y);
+  if(flag == 0 || x == y)
+  {
+    printf("\nINVALID INPUT\n");
+    exit(0);
+  }
+  n = x * y;
+  t = (x-1) * (y-1);
+  int k;
+  k = 0;
+  for(i = 2; i < t; i++)
+  {
+    if(t % i == 0)
+     continue;
     flag = prime(i);
-    if(flag == 1 && i != p && i != q) 
+    if(flag == 1 && i != x && i != y)
     {
-        e[key_RSA] = i;
-        flag = cd(e[key_RSA]);
+     e[k] = i;
+     flag = cd(e[k]);
     if(flag > 0)
     {
-        d[key_RSA] = flag;
-        key_RSA++;
+     d[k] = flag;
+     k++;
     }
-   if(key_RSA == 99)
+   if(k == 99)
     break;
    }
- } 
- 
+ }
 }
 long int cd(long int a)
 {
@@ -92,36 +117,41 @@ long int cd(long int a)
 }
 
 //function to encrypt the message
-void Encrypt_RSA()
+void encrypt()
 {
-  long int pt, ct, key = e[0], k, len;    
+  printf("\nENTER MESSAGE OR STRING TO ENCRYPT\n");
+  scanf("%s",msg);
+  for(i = 0; msg[i] != NULL; i++)
+    m[i] = msg[i];
+  
+  long int pt, ct, key = e[0], k, len;
   i = 0;
   len = strlen(msg);
   while(i != len)
   {
-        pt = m[i];
-        pt = pt - 96;
-        k = 1;
+    pt = m[i];
+    pt = pt - 96;
+    k = 1;
     for(j = 0; j < key; j++)
     {
-        k = k * pt; 
-        k = k % n;
+     k = k * pt;
+     k = k % n;
     }
    temp[i] = k;
    ct = k + 96;
-   C[i] = ct;    
+   en[i] = ct;
    i++;
   }
-  C[i] = -1;
-  printf("\nThe encrypted message: );
-  for(i = 0; C[i] != -1; i++)
-    printf("%c", C[i]);
+  en[i] = -1;
+  printf("\n\nTHE ENCRYPTED MESSAGE IS\n");
+  for(i = 0; en[i] != -1; i++)
+    printf("%c", en[i]);
 }
 
 //function to decrypt the message
 void decrypt()
 {
-  long int pt, ct, key = d[0], k;ew4
+  long int pt, ct, key = d[0], k;
   i = 0;
   while(en[i] != -1)
   {
@@ -136,9 +166,9 @@ void decrypt()
    m[i] = pt;
    i++;
   }
-  M[i] = -1;
-  printf("\nThe decrypted message:");
-  for(i = 0; M[i] != -1; i++)
-   printf("%c", M[i]);
+  m[i] = -1;
+  printf("\n\nTHE DECRYPTED MESSAGE IS\n");
+  for(i = 0; m[i] != -1; i++)
+   printf("%c", m[i]);
   printf("\n");
 }
